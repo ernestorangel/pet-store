6f86d40ee24f1da67cb8b7b90ac1909621cb0d5c
FernandoStentzler <stentzlerfer@hotmail.com>
2022-05-25 18:58:34 -0300

    implementação for checkout

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -1,5 +1,21 @@
+const path = require('path')
+const casinha = path.join('images', 'products', 'casinhaLuxoPreta.png')
 const produtos = [{
-    
+    titulo: "Casinha Luxo Preta",
+    descricao: "Esteja Sempre Pronto para levar seu Pet com voce",
+    quantidade: 2,
+    preco: 150.00,
+},
+{
+    titulo: "Tapete Higienico",
+    descricao: "Local Adequado para seu Pet Fazer necessidade",
+    quantidade: 1,
+    preco: 50.00
+},{
+    titulo: "Comedouro",
+    descricao: "Pote Ideal para seu pet se alimentar",
+    quantidade: 1,
+    preco: 20.00
 }]
 
 const cartController = {
@@ -7,7 +23,7 @@ const cartController = {
         res.render('cart', { title: 'Carrinho' });
     },
     checkout: (req,res,next) =>{
-        res.render('checkout', { title: 'Checkout' });
+        res.render('checkout', { title: 'Checkout', produtos: produtos });
     },
 }
 module.exports = cartController
