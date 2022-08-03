2f65c2ff13593cb6d36e11056f04faa3f97eb943
ernestorangel <ernestojrcjunior@gmail.com>
2022-08-02 21:45:02 -0300

    unfinished changes to product registration form

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,15 +1,15 @@
 const path = require('path')
 const {Product} = require('../database/models');
 
-async function getProduct(id) {
-  let product;
-  await Product.findByPk(id, {
-      attributes: ['id_product', 'name', 'price', 'img'],
-  }).then((data)=>{
-      product = data.dataValues;
-  });
-  return product
-}
+// function getProduct(id) {
+//   let product;
+//   Product.findByPk(id, {
+//       attributes: ['id_product', 'name', 'price', 'img'],
+//   }).then((data)=>{
+//       product = data.dataValues;
+//   });
+//   return product
+// }
 
 const productController = {
     search: (req, res) => {
@@ -20,6 +20,9 @@ const productController = {
       res.render('product2', {
         prod: prod,
       });
+    },
+    register: async (req, res) => {
+      res.render('productRegistration');
     }
 }
 module.exports = productController
--- src/routes/products.js
+++ src/routes/products.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const productsController = require('../controllers/productsController');
 
-router.get("/:id", productsController.view);
+router.get("/view/:id", productsController.view);
 router.get("/search/:word", productsController.search);
+router.get("/register", productsController.register);
 
 module.exports = router;
